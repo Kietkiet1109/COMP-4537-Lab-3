80b51151c52868c6a1d5e91358f54bd0b9e6c4cc
Kiet Ngo <144854673+Kietkiet1109@users.noreply.github.com>
2025-02-01 15:54:30 -0800

    Update en.js

--- lang/en/en.js
+++ lang/en/en.js
@@ -1,9 +1,11 @@
 module.exports = {
     hello: 'Hello %1, What a beautiful day. Server current date and time is',
-    getDateError: 'Make request',
+    getDateError: 'Error getting date',
     appended: 'Text appended to file.txt: ',
-    writeError: 'Make request',
-    readError: 'Make request',
+    writeError: 'Error writing',
+    readError: 'Error reading',
     noFile: 'No file specified.',
-    notFound: '404 Not Found'
+    notFound: '404 Not Found',
+    urlError: 'Invalid URL',
+    serverRunning: 'Server is running on port 8000'
 };
